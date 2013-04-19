7c9feb34a4b07949eb33c365a717ac7c45c6570c
axxe16 <grassi.alessandro@gmail.com>
2013-04-19 23:28:45 +0200

    update features
    - un nuovo font standard (edelsans)
    - integrazione iniziale del pannello di configurazione

--- js/jquery.path.story.js
+++ js/jquery.path.story.js
@@ -2,7 +2,7 @@
  * 
  * jQuery Path Story
  * 
- * Version 0.2 (08-04-2013)
+ * Version 0.3 (19-04-2013)
  * semanticstone.net
  *
  * Licensed under GPL license:
@@ -21,7 +21,8 @@
        
 // Default
   var defaults = {
-		debug: false
+		debug: false,
+		config: true
     };
             
   // # Utilizzo la funzione $.extend per "mergiare" gli oggetti
@@ -32,6 +33,8 @@
 		console.dir(book)
 		pageInfo(book);
 		linearNav(book); 
+		if(config)
+			config(book)
     });  
   
   //STAMPO IN OGNI PAGINA IL TITOLO DEL LIBRO, IL TITOLO DEL CAPITOLO IL NUMERO DEL CAPITOLO CORRENTE E L'AUTORE
@@ -58,6 +61,34 @@ function pageInfo(book)
 		$(this).prepend(titolo_capitolo);
 		$(this).append(capitolo);		
 	}); 
+}
+
+function config(book) {
+	var pages = $(book).children('section');
+	var paragraph = $(pages).children('p');
+	//inserisco il pulsante config
+	$(book).prepend('<div class="nav" id="configBookButton"></div>');
+	//inserisco il box di configurazione
+	$('body').prepend('<div class="wrapConfigBook"><div id="configBook"><div class="wrapInnerConfigBook"><div class="row dSelection"><span>Dimensione Testo</span><i class="less"></i><i class="plus"></i></div><!--<div class="row mSelection"><span>Margini</span><i class="less"></i><i class="plus"></i></div><div class="row iSelection"><span>Interlinea</span><i class="less"></i><i class="plus"></i></div>--></div></div></div>');
+//gestione dimensioni del testo
+plus(paragraph,16,23,7,'.dSelection .plus','fontSize');
+less(paragraph,16,10,6,'.dSelection .less','fontSize');
+
+	
+	//ricavo l'altezza del box di configurazione
+	var heightBox = '-' + $(".wrapConfigBook").height();
+	var heightBoxEnter = $(".wrapConfigBook").height();
+	//comportamento pulsante per visualizzare/nascondere il box
+	$("#configBookButton").click(function() {
+		if ($('#configBookButton.active').length) {
+			$(this).removeClass('active');
+			$(".wrapConfigBook").animate({top: parseInt(heightBox), opacity:0},300);
+		} else {
+			$(".wrapConfigBook").css({top: parseInt(heightBox),opacity:0 }).show();
+			$(".wrapConfigBook").animate({top: 0, opacity:1},400);
+			$(this).addClass('active');
+		}
+	});
 }
 
 function linearNav(book) {
@@ -96,6 +127,38 @@ function linearNav(book) {
 	});
 }
   
+
+//funzioni di controllo formattazione
+function plus(target,start,end,step,button,style) { 
+    //target -> elemento su cui agire
+    //start  -> valore di default
+    //end    -> valore massimo
+    //step   -> n° di step
+    //button -> elemento attuatore
+    var stepValue = Math.floor((end-start)/step);
+    $(button).click(function() {
+        var size = $(target).css(style);
+        size = size.substring(0, size.length - 2);
+        if (parseInt(size) < end) {
+            size = parseFloat(size) + parseInt(stepValue);
+            $(target).css(style,size);
+        }
+    }) 
+}
+
+function less(target,start,end,step,button,style) {
+    var stepValue = Math.floor((start-end)/step); 
+    $(button).click(function() {
+        var size = $(target).css(style);
+            size = size.substring(0, size.length - 2);        
+            if (parseInt(size) > end) {
+                size = parseFloat(size) - parseInt(stepValue);
+                $(target).css(style,size);
+            }
+    })
+}
+
+
   
   
   
